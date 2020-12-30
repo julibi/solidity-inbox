d4f04b1d9c6b717727530a850fe9e67a6b53e031
hyun.yi <hyun.yi@xing.com>
2020-12-30 15:10:01 +0100

    creates, compiles and deploy first simple contract

---
+++ compile.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+var input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+// module.exports = output.contracts["Inbox.sol"]["Inbox"].abi;
+// module.exports = output.contracts["Inbox.sol"]["Inbox"].evm.bytecode.object;
+module.exports = output.contracts["Inbox.sol"]["Inbox"];
